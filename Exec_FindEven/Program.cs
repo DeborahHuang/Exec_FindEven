fec3880da57b96f133b9a85cfbc493f7f23174dd
88693 <88693@LAPTOP-FS9ST1LD>
2022-10-24 23:14:14 +0800

    加入專案檔案。

---
+++ Exec_FindEven/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_FindEven
+{//找出[4, 15] 之間的所有偶數
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int startNumber = 4;
+			int endNumber = 15;
+
+			for (int i = startNumber; i <= endNumber; i++)
+			{
+				if (i % 2 == 0)
+				{
+					Console.WriteLine(i);
+				}
+			}
+		}
+	}
+}
